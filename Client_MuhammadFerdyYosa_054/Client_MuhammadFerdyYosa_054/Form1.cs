fd1255c5a997d394c98ba9e9afc81e2d7d908357
Muhammad Ferdy Yosa <55939744+FerdyYosa@users.noreply.github.com>
2020-10-31 15:21:27 +0700

    Add project files.

---
+++ Client_MuhammadFerdyYosa_054/Client_MuhammadFerdyYosa_054/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client_MuhammadFerdyYosa_054
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.MatematikaClient obj = new
+            ServiceReference1.MatematikaClient();
+            double hasilTambah = obj.Tambah(1, 2);
+            
+            double hasilKurang = obj.Kurang(3, 2);
+            
+            double hasilKali = obj.Kali(2, 2);
+            
+            double hasilBagi = obj.Bagi(2, 2);
+            
+            ServiceReference1.Koordinat a = new
+            ServiceReference1.Koordinat();
+            ServiceReference1.Koordinat b = new
+            ServiceReference1.Koordinat();
+            a.X = 7;
+            a.Y = 8;
+            b.X = 5;
+            b.Y = 6;
+            double selisihX = a.X - b.X;
+            double selisihY = a.Y - b.Y;
+            double jarak = Math.Sqrt(Math.Pow(selisihX, 2) +
+            Math.Pow(selisihY, 2));
+            
+        }
+    }
+}
---
+++ Server_MuhammadFerdyYosa_20180140054/Server_MuhammadFerdyYosa_20180140054/Form1.cs
@@ -0,0 +1,76 @@
+﻿using ServiceMtk_P2_054;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server_MuhammadFerdyYosa_20180140054
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                label2.Text = "Server ON";
+                label3.Text = "Klik OFF untuk Mematikan Server";
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                label3.Text = ex.Message;
+                label2.Text = "Server OFF";
+                label3.Text = "Klik ON untuk Menjalankan Server";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                label2.Text = "Server ON";
+                label3.Text = "Klik OFF untuk Mematikan Server";
+                hostObj.Close();
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                label3.Text = ex.Message;
+                label2.Text = "Server OFF";
+                label3.Text = "Klik ON untuk Menjalankan Server";
+                button1.Enabled = true;
+                button2.Enabled = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ServiceHost hostObj = null;
+            hostObj = null;
+            label2.Text = "Server OFF";
+            label3.Text = "Klik ON untuk Menjalankan Server";
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+    }
+}
